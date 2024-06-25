5847b984204bb695b7ff9c80c2af5cea095a8f13
Dhruv195 <dhruv.joshi.live@gmail.com>
2024-06-25 17:33:55 +0530

    [Project-2]=>Footer component and Prodcut-card intergrate

--- src/app/component/main/home/home.component.ts
+++ src/app/component/main/home/home.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FeaturedCardComponent } from 'src/app/shared/common/featured-card/featured-card.component';
 import { CategoryCarousulSectionComponent } from './category-carousul-section/category-carousul-section.component';
 import { CategorySectionComponent } from './category-section/category-section.component';
+import { FeaturedSectionComponent } from './featured-section/featured-section.component';
+import { FeaturedProductSectionComponent } from './featured-product-section/featured-product-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,FeaturedCardComponent,CategoryCarousulSectionComponent,CategorySectionComponent],
+  imports: [CommonModule,FeaturedSectionComponent,CategoryCarousulSectionComponent,CategorySectionComponent,FeaturedProductSectionComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,13 +1,59 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  navigationList = [
+    {
+      navigationLabel: 'Quick Link',
+      navigationLinks: [
+        {
+          label: 'Home',
+          link: '/home',
+        },
+        {
+          label: 'Our Shop',
+          link: '/shope',
+        },    {
+          label: 'Shopping Cart',
+          link: '/shopping-cart',
+        },    {
+          label: 'Checkout',
+          link: '/checkout',
+        },    {
+          label: 'Contact Us',
+          link: '/Contact',
+        }
+      ],
+    },
+    {
+      navigationLabel: 'My Account',
+      navigationLinks: [
+        {
+          label: 'Home',
+          link: '/home',
+        },
+        {
+          label: 'Our Shop',
+          link: '/shope',
+        },    {
+          label: 'Shopping Cart',
+          link: '/shopping-cart',
+        },    {
+          label: 'Checkout',
+          link: '/checkout',
+        },    {
+          label: 'Contact Us',
+          link: '/Contact',
+        }
+      ],
+    },
+  ];
 }
