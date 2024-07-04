4924cfd588b5932f4da2dc2a36cc05451a65b103
visha-2677 <93275047+visha-2677@users.noreply.github.com>
2024-07-04 09:02:02 +0530

    [MS-#60]=> Shop In Product Filter

--- src/app/component/main/user-profile/sidebar/sidebar.component.ts
+++ src/app/component/main/user-profile/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { PROFILE_ITEMS } from 'src/app/shared/constant/common-function';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,43 +11,7 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  profileUrls = [
-    {
-      title: 'Profile',
-      link: '/user-profile/edit-profile',
-      icon:'bi bi-person-fill text-primary fs-4'
-      
-    },
-    {
-      title: 'Change Password',
-      link: '/user-profile/change-password',
-      icon:"bi bi-lock-fill fs-4 text-primary"
-      
-    },
-    {
-      title: 'Shopping Cart',
-      link: '/user-profile/shopping-cart',
-      icon:'bi bi-cart-fill fs-4 text-primary'
-      
-    },
-    {
-      title: 'Wish List',
-      link: '/user-profile/wish-list',
-      icon:'bi bi-list-stars fs-4 text-primary'
-      
-    },
-    {
-      title: 'Order List',
-      link: '/user-profile/order-list',
-      icon:'bi bi-card-checklist fs-4 text-primary'
-    },
-    {
-      title: 'Sign Out',
-      link:'/home',
-      icon:'bi bi-box-arrow-right fs-4 text-primary'
-
-    }
-  ]
+  profileUrls = PROFILE_ITEMS;
 
   onProfileItemClick(item:any) {
     if (item == 'Sign Out') {
